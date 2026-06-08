a594e048031af0d91103ecf59c26c4cd58e0c9f7
shelarshriniwas <shelar.shriniwas@gmail.com>
2026-06-09 03:09:32 +0530

    Updated file

--- GIT/hello.sh
+++ GIT/hello.sh
@@ -1,1 +1,16 @@
-this is hell git program
+#!/bin/bash
+
+git init
+
+for day in $(seq 1 365)
+do
+    date=$(date -d "2025-01-01 +$((day-1)) days" +%Y-%m-%d)
+
+    echo "$date" >> contributions.txt
+
+    git add contributions.txt
+
+    GIT_AUTHOR_DATE="$date 12:00:00" \
+    GIT_COMMITTER_DATE="$date 12:00:00" \
+    git commit -m "Contribution $date"
+done
